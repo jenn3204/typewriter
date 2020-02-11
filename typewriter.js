12d92e8209c3eca539c7c8cc0850e7a2819a7739
jenniferjaque <jenn3204@stud.kea.dk>
2020-02-11 21:32:14 +0100

    typewriter working

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,20 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+const text = document.querySelector(".typewritten").textContent;
+let counter = 1;
+
+function start() {
+  document.querySelector(".typewritten").textContent = "";
+  document.querySelector(".typewritten").textContent = text.substring(0, counter);
+  counter++;
+
+  setTimeout(showLetters, 300);
+}
+
+function showLetters() {
+  if (text.length >= counter) {
+    start();
+  }
+}
